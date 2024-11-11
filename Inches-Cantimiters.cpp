132964d27239bd246a40b45ff87bcd314109a668
SmolCutiePie <fancoretop@gmail.com>
2024-11-11 22:28:16 +0300

    Add files via upload

---
+++ Inches-Cantimiters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int gallons, liters;
+    
+    cout << "Inches: " << endl;
+    cin >> gallons;
+    
+    liters = gallons * 2.54 ;
+    
+    cout << "Santimeters " << liters;
+    
+    return 0;
+    
+}
